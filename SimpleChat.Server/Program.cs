32a339ffce7d956d80c5e4e7c7e9a7299beea4cf
Starinin Andrey <starininandrey@gmail.com>
2022-11-30 19:20:19 +0300

    Add Server

---
+++ SimpleChat.Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var port = 8005;
+var endPoint = new IPEndPoint(ipAddress, port);
+
+var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listenerSocket.Bind(endPoint);
+listenerSocket.Listen();
+
+var clientSocket = listenerSocket.Accept();
+
+var messageSend = "Hello";
+var bufferSend = Encoding.UTF8.GetBytes(messageSend);
+clientSocket.Send(bufferSend);
+
+var messageRecieve = new StringBuilder();
+var bufferRecieve = new byte[256];
+var bytes = 0;
+do
+{
+    bytes = clientSocket.Receive(bufferRecieve);
+    messageRecieve.Append(Encoding.UTF8.GetString(bufferRecieve, 0, bytes));
+} while (clientSocket.Available > 0);
