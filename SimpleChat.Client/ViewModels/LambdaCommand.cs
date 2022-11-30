e51034707aea6e333a9c15e2ff10c2cf97a15a1e
Starinin Andrey <starininandrey@gmail.com>
2022-11-30 21:31:48 +0300

    Update Client (implementation MVVM)

---
+++ SimpleChat.Client/ViewModels/LambdaCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SimpleChat.Client.ViewModels;
+
+public class LambdaCommand : ICommand
+{
+    private readonly Predicate<object?> _canExecute;
+    private readonly Action<object?> _execute;
+
+    public LambdaCommand(Predicate<object?> canExecute, Action<object?> execute)
+    {
+        _canExecute = canExecute;
+        _execute = execute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void OnCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ SimpleChat.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using SimpleChat.TCPLib;
+
+namespace SimpleChat.Client.ViewModels;
+
+public class MainWindowViewModel : BaseNotification
+{
+    private string _message;
+    public string Message
+    {
+        get => _message;
+        set
+        {
+            SetField(ref _message, value);
+            SendMessageCommand.OnCanExecuteChanged();
+        }
+    }
+
+    public LambdaCommand SendMessageCommand { get; set; }
+
+    private TcpClient _tcp;
+
+    public MainWindowViewModel()
+    {
+        _message = string.Empty;
+        _tcp = new TcpClient("127.0.0.1", 8005);
+
+        SendMessageCommand = new LambdaCommand(
+            _ => !string.IsNullOrWhiteSpace(Message),
+            async _ =>
+            {
+                await _tcp.SendAsync(Message);
+            });
+    }
+}
