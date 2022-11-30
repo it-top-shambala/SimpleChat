7eedba705e31038d4bcf96a2b876117445cce6da
Starinin Andrey <starininandrey@gmail.com>
2022-11-30 20:37:32 +0300

    Update Server (add TCPLib)

--- SimpleChat.Server/Program.cs
+++ SimpleChat.Server/Program.cs
@@ -1,26 +1,22 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using SimpleChat.TCPLib;
 
-var ipAddress = IPAddress.Parse("127.0.0.1");
-var port = 8005;
-var endPoint = new IPEndPoint(ipAddress, port);
+var tcpServer = new TcpServer("127.0.0.1", 8005);
+tcpServer.Start();
 
-var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-listenerSocket.Bind(endPoint);
-listenerSocket.Listen();
-
-var clientSocket = listenerSocket.Accept();
-
-var messageSend = "Hello";
-var bufferSend = Encoding.UTF8.GetBytes(messageSend);
-clientSocket.Send(bufferSend);
-
-var messageRecieve = new StringBuilder();
-var bufferRecieve = new byte[256];
-var bytes = 0;
-do
+while (true)
 {
-    bytes = clientSocket.Receive(bufferRecieve);
-    messageRecieve.Append(Encoding.UTF8.GetString(bufferRecieve, 0, bytes));
-} while (clientSocket.Available > 0);
+    var client = await tcpServer.AcceptAsync();
+
+    await Task.Run(async () =>
+    {
+        while (true)
+        {
+            var receive = await client.ReceiveAsync();
+#if DEBUG
+            Console.WriteLine(receive);
+#endif
+        }
+    });
+}
+
+
